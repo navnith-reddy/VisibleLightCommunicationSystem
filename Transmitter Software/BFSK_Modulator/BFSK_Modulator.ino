123e109c4085a677e9d21a024d860aa93d121cc7
navnith-reddy <navnith.m.reddy@gmail.com>
2024-05-05 03:26:14 +1000

    Integrated schematics completed

--- Transmitter Software/BFSK_Modulator/BFSK_Modulator.ino
+++ Transmitter Software/BFSK_Modulator/BFSK_Modulator.ino
@@ -12,29 +12,28 @@ const PROGMEM uint16_t bit_zero[21] =
 };
 */
 
+/*
 // 95.7 kHz
 const PROGMEM uint16_t bit_zero[8] =
 {
 390,  665,  780,  665,  390,  114,    0,  114
 };
-
-
-
-/*
-const PROGMEM uint16_t bit_zero[22] =
-{
- 390,  499,  600,  684,  744,  776,  776,  744, 
- 684,  600,  499,  390,  280,  179,   95,   35, 
-   3,    3,   35,   95,  179,  280
-};
-
-const PROGMEM uint16_t bit_one[17] = 
-{
- 390,  530,  652,  739,  778,  765,  701,  595, 
- 461,  318,  184,   78,   14,    1,   40,  127, 
- 249
-};
 */
+
+
+
+const PROGMEM uint16_t bit_zero[16] =
+{
+511,  706,  872,  983, 1022,  983,  872,  706, 
+ 511,  315,  149,   38,    0,   38,  149,  315
+};
+
+const PROGMEM uint16_t bit_one[16] = 
+{
+511,  872, 1022,  872,  511,  149,    0,  149, 
+ 510,  872, 1022,  872,  511,  149,    0,  149
+};
+
 SPISettings settings(20000000, MSBFIRST, SPI_MODE0);
 
 void sendDACValue(unsigned int value) {
@@ -63,9 +62,12 @@ void setup() {
 char data = 0xA3;
 
 void loop() {
-
-  //for (int i = 0; i < 22; i++) {sendDACValue (bit_zero[i]);}
-  for (int i = 0; i < 10; i++) {sendDACValue (bit_one[i]);}
+  sendDACValue(1023);
+  delay(100);
+  sendDACValue(0);
+  delay(100);
+  //for (int i = 0; i < 16; i++) {sendDACValue (bit_zero[i]);}
+  //for (int i = 0; i < 16; i++) {sendDACValue (bit_one[i]);}
 
   // Modulator
   /*for (int i = 7; i >= 0; i--) {
